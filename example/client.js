c60d36b98a9e73ed3367631699eba6350c335bf3
Antoine Pous <gecko@dvp.io>
2017-09-28 00:02:43 +0200

    Rewrite example with better use cases

--- example/client.js
+++ example/client.js
@@ -1,77 +1,91 @@
-'use strict'
-let log = require('npmlogger')
-let TS3Client = require('../teamspeak3-client')
-let TS3Utils = require('teamspeak3-utils')
-let TS3Definitions = require('teamspeak3-utils/definitions')
+"use strict";
+
+let TS3Client = require("../teamspeak3-client");
+let TS3Definitions = require("teamspeak3-utils/definitions");
 
 // Client configuration
-let host = '127.0.0.1'
-let port = 10011
-let login = 'serveradmin'
-let pass = 'myPassword'
-let whitelisted = false
+let host = '127.0.0.1';
+let port = 10011;
+let login = 'serveradmin';
+let pass = 'fernand';
+let whitelisted = true;
 
-log.fileLevel = 'silent'
+// Listen for client enter in query client view
+TS3Client.on("cliententerview", function(clients) {
+    clients.forEach(client => {
+        console.log(client.client_nickname + ' join the server !');
 
-TS3Client.on("error", function(prefix, id, msg) {
-  log.error(prefix, id, msg)
-})
+        // Send welcome message to the client
+        TS3Client.execute('hi', [client.clid, client.client_nickname], (err, rows, raw) => {
+            if(err.id > 0) console.log(err.msg);
+        });
+    });
+});
+
+// Listen for client left query client view
+TS3Client.on("clientleftview", function(clients) {
+    clients.forEach(client => {
+        console.log('A client left the server !')
+    });
+});
+
+// Listen text messages
+TS3Client.on("textmessage", function(msg) {
+    msg.forEach(entry => {
+        // If we received `!unregister` from private messages we unregister from all events
+        if(entry.msg === "!unregister" && entry.targetmode === TS3Definitions.TextMessageTarget_CLIENT) TS3Client.notifyUnregister();
+    });
+});
+
+TS3Client.on("error", function(prefix, entry) {
+    console.error(prefix, entry)
+});
 
 TS3Client.on("info", function(prefix, entry) {
-  log.info(prefix, entry)
-})
+    console.info(prefix, entry)
+});
 
 TS3Client.on("warn", function(prefix, entry) {
-  log.warn(prefix, entry)
-})
+    console.warn(prefix, entry)
+});
 
 TS3Client.on("verbose", function(prefix, entry) {
-  log.verbose(prefix, entry)
-})
+    console.debug(prefix, entry)
+});
 
 // Prepare some usefull queries
-TS3Client.prepare('login', 'login client_login_name=%s client_login_password=%s')
-TS3Client.prepare('kickFromServer', 'clientkick clid=%d reasonid=' + TS3Definitions.REASON_KICK_SERVER + ' reasonmsg=%s')
-TS3Client.prepare('usePort', 'use port=%d')
+TS3Client.prepare('login', 'login client_login_name=%s client_login_password=%s');
+TS3Client.prepare('kickFromServer', `clientkick clid=%d reasonid=${TS3Definitions.REASON_KICK_SERVER} reasonmsg=%s`);
+TS3Client.prepare('usePort', 'use port=%d');
+TS3Client.prepare('hi', `sendtextmessage targetmode=${TS3Definitions.TextMessageTarget_CLIENT} target=%d msg=Hi\\s%s!`);
 
 // Wait for the connection
 TS3Client.on("connected", function() {
 
-  // Login with account (see prepared query)
-  TS3Client.execute('login', [login, pass], function(err, res, query) {
+    //let [cmd, time] = TS3Client.antiFlood(10, 3);
+    //console.log(`The new antiflood policy is ${cmd} in ${time}s`)
 
-    // If we have an error
-    if(err.id > 0) {
-      log.error(err.msg) // log the error (see npmlogger)
-      return // Don't continue
-    }
+    // Login with account (see prepared query)
+    TS3Client.executeNow('login', [login, pass], function(err, res, query) {
 
-    // Use virtual server
-    TS3Client.execute('usePort', [9987], function(err, res, query) {
+        // If we have an error
+        if(err.id > 0) return;
 
-      // If we have an error
-      if(err.id > 0) {
-        log.error(err.msg) // log the error (see npmlogger)
-        return // Don't continue
-      }
+        // Use virtual server
+        TS3Client.executeNow('usePort', [9987], function(err, res, query) {
 
-      TS3Client.query('servernotifyregister', {event:'server'}, [], function(err, rows, query) {
-        if(err.id > 0) {
-          log.error(err.msg) // log the error (see npmlogger)
-          return // Don't continue
-        }
+            // If we have an error
+            if(err.id > 0) return;
 
-        TS3Client.on("cliententerview", function(clients) {
-          for(let i = 0; i < clients.length; i++) {
-            console.log(clients[i].client_nickname + ' join the server !')
-          }
+            // Register to server events
+            TS3Client.notifyRegister("server");
+            TS3Client.notifyRegister("textprivate");
+
         })
-      })
+
     })
 
-  })
-
-})
+});
 
 // Init the client
-TS3Client.connect(host, port, whitelisted)
+TS3Client.connect(host, port, whitelisted);
